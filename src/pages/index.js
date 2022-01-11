c74fedc4b126d9ecdf7cfcbab2403dfa3351217d
edwardtran97 <thuan.tranquoc0902@gmail.com>
2022-01-11 14:09:02 +0700

    Update thumnail

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,7 +62,7 @@ function Home() {
   const { siteConfig = {} } = context;
   return (
     <Layout
-      imgUrl={`https://images.unsplash.com/photo-1630569470960-ec1e8c077d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80`}
+      imageUrl={`https://images.unsplash.com/photo-1630569470960-ec1e8c077d6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80`}
       title={`Homepage`}
       description="Crypto Hunter Blog - Hành trình tìm kiếm, trải nghiệm cùng những bài học đắt giá trong đầu tư Crypto">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
