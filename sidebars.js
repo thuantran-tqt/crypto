45671af3d1511ba66a7346285af7143381ca9a8b
edwardtran97 <thuan.tranquoc0902@gmail.com>
2022-01-11 11:11:47 +0700

    Update sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,19 @@
 module.exports = {
   someSidebar: {
-    'Nhật ký đầu tư': ['nhat-ky-dau-tu-01122021'],
-    'Kiến thức': ['js-overview'],
-    'Phân tích': ['git-blog'],
+    'Nhật ký đầu tư': 
+    [
+      'git-blog',
+      'nhat-ky-dau-tu-01122021',
+    ],
+    'Kiến thức': 
+    [
+      'js-overview',
+      'nhat-ky-dau-tu-01122021',
+    ],
+    'Phân tích': 
+    [
+      'git-blog',
+      'js-overview',
+    ],
   },
 };
